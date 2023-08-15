1a753add88291c311ee7a23ed9d904f0ae3de75f
cleltonhenrique <cleltonh@gmail.com>
2023-08-15 13:55:12 -0300

    Ajustes

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
+from todo_list import ToDoList
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+class MainApp:
 
+    def __init__(self):
+        self.todo_list = ToDoList()
+
+    def run(self):
+        while True:
+            print("1 - Adicionar tarefa")
+            print("2 - Remover tarefa")
+            print("3 - Mostrar tarefas")
+            print("4 - Sair")
+            option = int(input("Escolha uma opção: "))
+
+            if option == 1:
+                task = input("Digite a tarefa: ")
+                self.todo_list.add_task(task)
+            elif option == 2:
+                task = input("Digite a tarefa: ")
+                self.todo_list.remove_task(task)
+            elif option == 3:
+                self.todo_list.show_tasks()
+            elif option == 4:
+                break
+            else:
+                print("Opção inválida!")
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    main_app = MainApp()
+    main_app.run()
--- todo_list.py
+++ todo_list.py
@@ -20,28 +20,3 @@ class ToDoList:
         else:
             print("A lista está vazia!")
 
-def main():
-    todo_list = ToDoList()
-
-    while True:
-        print("1 - Adicionar tarefa")
-        print("2 - Remover tarefa")
-        print("3 - Mostrar tarefas")
-        print("4 - Sair")
-        option = int(input("Escolha uma opção: "))
-
-        if option == 1:
-            task = input("Digite a tarefa: ")
-            todo_list.add_task(task)
-        elif option == 2:
-            task = input("Digite a tarefa: ")
-            todo_list.remove_task(task)
-        elif option == 3:
-            todo_list.show_tasks()
-        elif option == 4:
-            break
-        else:
-            print("Opção inválida!")
-
-if __name__ == '__main__':
-    main()
